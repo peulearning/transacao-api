9bfc386810da5fcc959a72ebe50f5e06e4654018
Pedro <peuhenry2018@gmail.com>
2025-02-04 17:12:22 -0300

    Adicionar / Limpar Implementados

--- transacao-api/src/main/java/com/inovasimples/transacao_api/bussiness/services/TransacaoService.java
+++ transacao-api/src/main/java/com/inovasimples/transacao_api/bussiness/services/TransacaoService.java
@@ -20,13 +20,20 @@ public class TransacaoService {
     private static final Logger log = LoggerFactory.getLogger(TransacaoService.class);
     private final List<TransacaoRequestDTO> listaTransacoes = new ArrayList<>();
 
-
     public void adicionarTransacoes(TransacaoRequestDTO dto){
         log.info("Iniciado o processamento de gravar transcões.");
         if(dto.dataHora().isAfter(OffsetDateTime.now())){
             log.error("Data e hora maiores que a atual");
             throw new UnprocessableEntity("Data e Hora, maiores que as datas atuais.");
         }
-        if(dto.valor())
+        if(dto.valor() < 0){
+            log.error("Valor não pode ser menor que 0");
+            throw new UnprocessableEntity("Valor não pode ser menor que 0");
+        }
+        listaTransacoes.add(dto);
+    }
+
+    public void limparTransacoes(){
+        listaTransacoes.clear();
     }
 }
